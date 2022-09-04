b536f861362ed0ab223172768bffc778fabfb463
CanhHuy <huycanh14@gmail.com>
2022-09-04 20:59:28 +0700

    update

--- src/TuyenSinh-api.Application/Features/_Template/Dtos/_TemplateExportVm.cs
+++ src/TuyenSinh-api.Application/Features/_Template/Dtos/_TemplateExportVm.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using TuyenSinh_api.Domain.Common;
+using System.ComponentModel.DataAnnotations;
+//using System.Text.Json.Serialization;
+using Newtonsoft.Json;
 
 namespace TuyenSinh_api.Application.Features._Template.Dtos
 {
